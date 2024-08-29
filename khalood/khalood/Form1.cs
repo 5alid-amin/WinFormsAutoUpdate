eacc02f480f3ceba9055f17784d1efc228d1044e
5aled <5aled.amin.sn@gmail.com>
2024-08-29 23:29:41 +0300

    first commit

---
+++ khalood/khalood/Form1.cs
@@ -0,0 +1,15 @@
+namespace khalood
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("i'm the first virsion");
+        }
+    }
+}
